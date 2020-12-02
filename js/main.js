d1cbb9b5876d1eae9f5d3915182606eab0a70069
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-01 20:55:19 -0800

    completed issue-1-create-profile

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $form = document.querySelector("form")
+var $profileImage = document.querySelector(".profile-image")
+
+$form.addEventListener("input", function (event){
+  if (event.target===$form.elements.avatarUrl){
+    $profileImage.setAttribute("src",$form.elements.avatarUrl.value)
+  }
+})
+
+$form.addEventListener("submit",function(event){
+  event.preventDefault()
+  data.profile.avatarUrl=$form.elements.avatarUrl.value
+  data.profile.username=$form.elements.username.value
+  data.profile.fullName=$form.elements.fullName.value
+  data.profile.location=$form.elements.location.value
+  data.profile.bio=$form.elements.bio.value
+  var dataJSON = JSON.stringify(data)
+  localStorage.setItem("data", dataJSON)
+  $form.reset()
+  $profileImage.setAttribute("src","./images/placeholder-image-square.jpg")
+})
