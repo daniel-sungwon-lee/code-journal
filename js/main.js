482e674c5a8db3b909e0c79d9c09beb504f59eef
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-03 11:27:30 -0800

    completed until step 3 of issue #3

--- js/main.js
+++ js/main.js
@@ -27,48 +27,75 @@ var $section = document.querySelector("section")
 
 function renderProfile (profile){
   var section = document.createElement("section")
+
   var h1 = document.createElement("h1")
   h1.textContent=profile.fullName
   section.appendChild(h1)
+
   var div = document.createElement("div")
   div.setAttribute("class","row-half")
   section.appendChild(div)
+
   var div2 = document.createElement("div")
   div2.setAttribute("class","image-frame")
   div.appendChild(div2)
+
   var img = document.createElement("img")
   img.setAttribute("src",profile.avatarUrl)
   img.setAttribute("class","profile-image")
   img.setAttribute("alt","Avatar Image")
   div2.appendChild(img)
+
   var div3 = document.createElement("div")
   div3.setAttribute("class","text-inputs")
   div.appendChild(div3)
+
   var div4 = document.createElement("div")
   div4.setAttribute("class","texts")
   div3.appendChild(div4)
+
   var div5 = document.createElement("div")
   div5.setAttribute("class","text-with-icons")
   div4.appendChild(div5)
+
   var i = document.createElement("i")
   i.setAttribute("class","fas fa-user-circle icon")
   div5.appendChild(i)
+
   var p = document.createElement("p")
   p.textContent=profile.username
   div5.appendChild(p)
+
   var div6 = document.createElement("div")
   div6.setAttribute("class","text-with-icons")
   div4.appendChild(div6)
+
   var i2 = document.createElement("i")
   i2.setAttribute("class","fas fa-compass icon")
   div6.appendChild(i2)
+
   var p2 = document.createElement("p")
   p2.textContent=profile.location
   div6.appendChild(p2)
+
   var p3 = document.createElement("p")
   p3.setAttribute("class","bio")
   p3.textContent=profile.bio
   div4.appendChild(p3)
+
+  var divRowButton = document.createElement("div")
+  divRowButton.setAttribute("class","row-button")
+  section.appendChild(divRowButton)
+
+  var anchorEditProfile = document.createElement("a")
+  anchorEditProfile.setAttribute("href","#")
+  anchorEditProfile.setAttribute("data-view","edit-profile")
+  divRowButton.appendChild(anchorEditProfile)
+
+  var buttonEditProfile = document.createElement("button")
+  buttonEditProfile.textContent="Edit"
+  anchorEditProfile.appendChild(buttonEditProfile)
+
   return section
 }
 
